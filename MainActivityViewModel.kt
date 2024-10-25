7d7619a76904d2d2df14e07219ee9e96f4c64f3a
santigm12 <159147660+santigm12@users.noreply.github.com>
2024-10-25 10:52:05 +0200

    Delete MainActivityViewModel.kt

--- MainActivityViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.actividad3_3
-
-import androidx.lifecycle.ViewModel
-
-class MainActivityViewModel : ViewModel() {
-    var segundosElegidos = 0
-    var tiempoBase:Long = 0
-    var cronometroActivo = false
-}
